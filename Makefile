9d1abad1bc9aaf224df588418b7a61b86b43c21e
Sebastian Urban <surban@surban.net>
2025-07-04 00:08:43 +0200

    environment variables

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 memstop.so: memstop.c Makefile
 	gcc -s -Os -shared -fPIC -o memstop.so memstop.c
 
+install: memstop.so
+	install -m 755 memstop.so /usr/local/lib
+
 clean:
 	rm -f memstop.so
--- memstop.c
+++ memstop.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -38,15 +39,43 @@ static int get_memory_info(struct MemoryInfo *mem_info) {
 void __attribute__((constructor)) memstop(void) {
   struct MemoryInfo memory;
   long required_kb;
+  long percent = 10;
+  char *env;
+  bool verbose = false;
+  bool informed = false;
+
+  env = getenv("MEMSTOP_PERCENT");
+  if (env) {
+    percent = atol(env);
+    if (percent < 0)
+      percent = 0;
+    if (percent > 100)
+      percent = 100;
+  }
+
+  env = getenv("MEMSTOP_VERBOSE");
+  if (env)
+    verbose = true;
+
+  if (verbose)
+    fprintf(stderr, "==== memstop loaded ====\n");
 
   while (true) {
     if (get_memory_info(&memory) != 0)
       return;
 
-    required_kb = memory.total_kb * 10 / 100;
+    required_kb = memory.total_kb * percent / 100;
     if (memory.available_kb >= required_kb)
       break;
 
+    if (verbose && !informed) {
+      fprintf(stderr, "==== memstop hold ====\n");
+      informed = true;
+    }
+
     usleep(10000);
   }
+
+  if (verbose && informed)
+    fprintf(stderr, "==== memstop release ====\n");
 }
